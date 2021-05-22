ccd4d3ef83989e7d207299ff804dbf94280cc830
carlej <carlej@oregonstate.edu>
2021-05-22 00:01:08 -0700

    Update test_leapYear.py

--- test_leapYear.py
+++ test_leapYear.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
-from tmp import lpYear.py
+from tmp import lpYear
 
 class testCaseUnit(unittest.TestCase):
     def test_good(self):
-        self.assertEqual(leapYear.leapYear(2020),True)
+        self.assertEqual(lpYear.leapYear(2020),True)
     def test_bad(self):
-        self.assertEqual(wordc.wordcount(2021),False)
+        self.assertEqual(lpYear.leapYear(2021),False)
         
 
 if __name__ == '__main__':
